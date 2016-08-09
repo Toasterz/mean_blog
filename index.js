725eef305b2a84e25279d5a49c7b6a9912ccd9ae
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-08-08 20:05:50 -0400

    Initial setup with test Angular

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cors = require('cors');
+
+var server = express();
+var port = process.env.PORT || 8080;
+
+server.use(express.static(__dirname + '/public'));
+server.use(cors());
+
+server.get('/', function(req, res, next)
+{
+  res.sendFile('index.html', {root: __dirname + '/public/html/'});
+});
+
+server.listen(port, function()
+{
+  console.log('Listening on port ' + port);
+});
