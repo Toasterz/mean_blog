e2a3876c8bff6cf96f311c4d1fa9407cc0772700
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-08-08 21:02:34 -0400

    started routes

--- public/js/Controllers/SimpleController.js
+++ public/js/Controllers/SimpleController.js
@@ -2,10 +2,28 @@
   angular.module('ngBlog')
           .controller('SimpleController', SimpleController)
 
-  SimpleController.$inject = ['$scope'];
+  SimpleController.$inject = ['$scope', 'UserService'];
 
-  function SimpleController($scope)
+  function SimpleController($scope, UserService)
   {
     $scope.message = "working Angular";
+    UserService.getAllUsers()
+              .then(function(response)
+            {
+              console.log(response);
+            });
+
+    var userObj =
+    {
+      firstName: 'Lil',
+      lastName: 'Jon',
+      age: 41,
+      email: 'andtheeast@sideboys.com'
+    };
+    UserService.createUser(userObj)
+                .then(function(response)
+              {
+                console.log(response);
+              });
   }
 }());
